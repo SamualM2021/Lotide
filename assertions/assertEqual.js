065777874827f984356f03c119f24a47973c7537
SamualM2021 <smartelli@zoho.com>
2021-11-10 09:55:54 -0500

    Reorganized JS files in lotide

---
+++ assertions/assertEqual.js
@@ -0,0 +1,24 @@
+/**
+ * This function asserts if the actual value is equivalent to the
+ * expected value.
+ * @param {the actual value} actual
+ * @param {the expected value} expected
+ */
+const assertEqual = function(actual, expected) {
+  const assertionPassedMessage = `💚💚💚 Assertion Passed: ${actual} === ${expected}`;
+  const assertionFailedMessage = `💔💔💔 Assertion Failed: ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log(assertionPassedMessage);
+  } else {
+    console.log(assertionFailedMessage);
+  }
+};
+
+const runTests = function() {
+  assertEqual("Lighthouse Labs", "Bootcamp");
+  assertEqual("Matching Text", "Matching Text");
+  assertEqual(1, 1);
+  assertEqual(1, 14);
+};
+
+runTests();
