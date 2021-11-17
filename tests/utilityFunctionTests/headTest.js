0b2b3a55a838ffbb8f97e46d78853cd9ec0cfd82
SamualM2021 <smartelli@zoho.com>
2021-11-17 10:34:30 -0500

    Refactored headTest to use Mocha and Chai

--- tests/utilityFunctionTests/headTest.js
+++ tests/utilityFunctionTests/headTest.js
@@ -1,11 +1,27 @@
-const assertions = require("../../assertions");
+const assert = require('chai').assert;
 const utilities = require("../../utilityFunctions");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(utilities.getHead([1, 2, 3]), 1);
+  });
 
-const runTests = function() {
-  assertions.assertEqual(utilities.getHead(null), undefined);
-  assertions.assertEqual(utilities.getHead([]), undefined);
-  assertions.assertEqual(utilities.getHead([5, 6, 7]), 5);
-  assertions.assertEqual(utilities.getHead(["Hello", "Lighthouse", "Labs"]), "Hello");
-};
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(utilities.getHead(['5']), '5');
+  });
 
-runTests();
+  it("returns undefined for null", () => {
+    assert.strictEqual(utilities.getHead(null), undefined);
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(utilities.getHead([]), undefined);
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(utilities.getHead([5, 6, 7]), 5);
+  });
+
+  it("returns Hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(utilities.getHead(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
