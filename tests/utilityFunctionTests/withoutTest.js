0e5a2b76a47c9c4a75d8f7bf1a64d7bd15aab550
SamualM2021 <smartelli@zoho.com>
2021-11-20 10:06:45 -0500

    Refactored withOutTest to use Mocha and Chai

--- tests/utilityFunctionTests/withoutTest.js
+++ tests/utilityFunctionTests/withoutTest.js
@@ -1,16 +1,22 @@
-const assertions = require("../../assertions");
+const assert = require('chai').assert;
 const utilities = require("../../utilityFunctions");
+const words = ["hello", "world", "lighthouse"];
+utilities.without(words, ["lighthouse"]); // no need to capture return value for this test case
 
-const runTests = function() {
-  assertions.assertArraysEqual(utilities.without([1, 2, 3], [1]), [2, 3]);
-  assertions.assertArraysEqual(utilities.without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
-  assertions.assertArraysEqual(utilities.without(null, [1]), null);
-  assertions.assertArraysEqual(utilities.without([1,2], null), [1,2]);
-  //Does Original Array remain unchanged?
-  const words = ["hello", "world", "lighthouse"];
-  utilities.without(words, ["lighthouse"]); // no need to capture return value for this test case
-  // Make sure the original array was not altered by the without function
-  assertions.assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-};
-
-runTests();
+describe("#without", () => {
+  it("[1, 2 , 3] without 1 should return [2, 3]", () => {
+    assert.deepEqual(utilities.without([1, 2, 3], [1]), [2, 3]);
+  });
+  it("['1', '2', '3'] without [1 , 2 , '3'] should return ['1', '2'", () => {
+    assert.deepEqual(utilities.without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+  });
+  it("null input should return null", () => {
+    assert.deepEqual(utilities.without(null, [1]), null);
+  });
+  it("[1, 2] without null should return [1, 2]", () => {
+    assert.deepEqual(utilities.without([1,2], null), [1,2]);
+  });
+  it("does words remain unchanged", () => {
+    assert.deepEqual(words, ["hello", "world", "lighthouse"]);
+  });
+});
