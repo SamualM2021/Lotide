ca64f804178eb570ccef22484d740576c03ecaac
SamualM2021 <smartelli@zoho.com>
2021-11-20 09:41:55 -0500

    Refactored eqObjectsTest and findKeyByValueTest to use Mocha and Chai

--- tests/utilityFunctionTests/eqObjectsTest.js
+++ tests/utilityFunctionTests/eqObjectsTest.js
@@ -1,26 +1,45 @@
-const assertions = require("../../assertions");
+const assert = require('chai').assert;
 const utilities = require("../../utilityFunctions");
 
-const runTests = function() {
-  const firstTestObject = { a: "1", b: "2" };
-  const secondTestObject = { b: "2", a: "1" };
-  const thirdTestObject = { a: "1", b: "2", c: "3" };
-  const firstDeepTestObject = { a: "1", b: { message: "YES"}, c: "3" };
-  const secondDeepTestObject = { a: "1", b: { message: "YES"}, c: "3" };
-  const thirdDeepTestObject = { a: "1", b: { message: "NO"}, c: "3" };
-  const firstArrayTestObject = { c: "1", d: ["2", 3] };
-  const secondArrayTestObject = { d: ["2", 3], c: "1" };
-  const thirdArrayTestObject = { c: "1", d: ["2", 3, 4] };
-  assertions.assertTrue(utilities.eqObjects(firstTestObject, secondTestObject));
-  assertions.assertFalse(utilities.eqObjects(firstTestObject, thirdTestObject));
-  assertions.assertFalse(utilities.eqObjects(null, []));
-  assertions.assertTrue(utilities.eqObjects(null, null));
-  assertions.assertFalse(utilities.eqObjects({}, {}));
-  assertions.assertFalse(utilities.eqObjects(firstTestObject, thirdDeepTestObject));
-  assertions.assertTrue(utilities.eqObjects(firstDeepTestObject, secondDeepTestObject));
-  assertions.assertFalse(utilities.eqObjects(firstDeepTestObject, thirdDeepTestObject));
-  assertions.assertTrue(utilities.eqObjects(firstArrayTestObject, secondArrayTestObject));
-  assertions.assertFalse(utilities.eqObjects(firstArrayTestObject, thirdArrayTestObject));
-};
+const firstTestObject = { a: "1", b: "2" };
+const secondTestObject = { b: "2", a: "1" };
+const thirdTestObject = { a: "1", b: "2", c: "3" };
+const firstDeepTestObject = { a: "1", b: { message: "YES"}, c: "3" };
+const secondDeepTestObject = { a: "1", b: { message: "YES"}, c: "3" };
+const thirdDeepTestObject = { a: "1", b: { message: "NO"}, c: "3" };
+const firstArrayTestObject = { c: "1", d: ["2", 3] };
+const secondArrayTestObject = { d: ["2", 3], c: "1" };
+const thirdArrayTestObject = { c: "1", d: ["2", 3, 4] };
 
-runTests();
+describe("#eqObjects", () => {
+  it("{ a: '1', b: '2' } should equal {  b: '2', a: '1' }", () => {
+    assert.isTrue(utilities.eqObjects(firstTestObject, secondTestObject));
+  });
+  it("null should equal null", () => {
+    assert.isTrue(utilities.eqObjects(null, null));
+  });
+  it("{ a: '1', b: { message: 'YES'}, c: '3 } should equal { a: '1', b: { message: 'YES'}, c: '3 }", () => {
+    assert.isTrue(utilities.eqObjects(firstDeepTestObject, secondDeepTestObject));
+  });
+  it("{c: '1', d: ['2', 3] } should equal {d: ['2', 3], c: '1' } ", () => {
+    assert.isTrue(utilities.eqObjects(firstArrayTestObject, secondArrayTestObject));
+  });
+  it("{ a: '1', b: '2' } should equal { a: '1', b: '2', c= '3'}", () => {
+    assert.isFalse(utilities.eqObjects(firstTestObject, thirdTestObject));
+  });
+  it("null should not equal an empty array", () => {
+    assert.isFalse(utilities.eqObjects(null, []));
+  });
+  it("two empty objects are not equal", () => {
+    assert.isFalse(utilities.eqObjects({}, {}));
+  });
+  it("{ a: '1', b: '2' } should not equal { a: '1', b: { message: 'NO'}, c: '3 }", () => {
+    assert.isFalse(utilities.eqObjects(firstTestObject, thirdDeepTestObject));
+  });
+  it("{ a: '1', b: { message: 'YES'}, c: '3 } should not equal { a: '1', b: { message: 'NO'}, c: '3 }", () => {
+    assert.isFalse(utilities.eqObjects(firstDeepTestObject, thirdDeepTestObject));
+  });
+  it("{c: '1', d: ['2', 3] } should not equal {c: '1', d: ['2', 3, 4] } should equal", () => {
+    assert.isFalse(utilities.eqObjects(firstArrayTestObject, thirdArrayTestObject));
+  });
+});
--- tests/utilityFunctionTests/findKeyByValueTest.js
+++ tests/utilityFunctionTests/findKeyByValueTest.js
@@ -1,4 +1,4 @@
-const assertions = require("../../assertions");
+const assert = require('chai').assert;
 const utilities = require("../../utilityFunctions");
 
 const bestTVShowsByGenre = {
@@ -9,15 +9,29 @@ const bestTVShowsByGenre = {
   canadianSciFi: "Orphan Black"
 };
 
-const runTests = function() {
-  assertions.assertEqual(utilities.findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-  assertions.assertEqual(utilities.findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
-  assertions.assertEqual(utilities.findKeyByValue(bestTVShowsByGenre, "Fairy Tail"), undefined);
-  assertions.assertEqual(utilities.findKeyByValue([], "The Wire"), undefined);
-  assertions.assertEqual(utilities.findKeyByValue(null, "The Wire"), undefined);
-  assertions.assertEqual(utilities.findKeyByValue(bestTVShowsByGenre, null), undefined);
-  assertions.assertEqual(utilities.findKeyByValue(bestTVShowsByGenre, "Resident Alien"), "comedySciFi");
-  assertions.assertEqual(utilities.findKeyByValue(bestTVShowsByGenre, "Orphan Black"), "canadianSciFi");
-};
-
-runTests();
+describe("#findKeyByValue", () => {
+  it("Brooklyn 99 should be found to be a comedy", () => {
+    assert.equal(utilities.findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  });
+  it("The Expanse should be found to be a scifi", () => {
+    assert.equal(utilities.findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+  });
+  it("Fairy Tail should return undefined", () => {
+    assert.equal(utilities.findKeyByValue(bestTVShowsByGenre, "Fairy Tail"), undefined);
+  });
+  it("an empty array should return undefined", () => {
+    assert.equal(utilities.findKeyByValue([], "The Wire"), undefined);
+  });
+  it("null object should return undefined", () => {
+    assert.equal(utilities.findKeyByValue(null, "The Wire"), undefined);
+  });
+  it("a null key should return undefined", () => {
+    assert.equal(utilities.findKeyByValue(bestTVShowsByGenre, null), undefined);
+  });
+  it("Resident Alien should return comedySciFi", () => {
+    assert.equal(utilities.findKeyByValue(bestTVShowsByGenre, "Resident Alien"), "comedySciFi");
+  });
+  it("Orphan Black should return canadianSciFi", () => {
+    assert.equal(utilities.findKeyByValue(bestTVShowsByGenre, "Orphan Black"), "canadianSciFi");
+  });
+});
