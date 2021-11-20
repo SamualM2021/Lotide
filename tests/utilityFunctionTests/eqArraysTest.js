9274992746d470ddad4af248c0c4664152bec7f7
SamualM2021 <smartelli@zoho.com>
2021-11-20 09:11:22 -0500

    Refactored eqArraysTest to use Mocha and Chai

--- tests/utilityFunctionTests/eqArraysTest.js
+++ tests/utilityFunctionTests/eqArraysTest.js
@@ -1,14 +1,26 @@
-const assertions = require("../../assertions");
+const assert = require('chai').assert;
 const utilities = require("../../utilityFunctions");
 
-const runTests = function() {
-  assertions.assertEqual(utilities.eqArrays([1, 2, 3], [1, 2, 3]), true);
-  assertions.assertEqual(utilities.eqArrays([1, 2, 3], [3, 2, 1]), false);
-  assertions.assertEqual(utilities.eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-  assertions.assertEqual(utilities.eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-  assertions.assertEqual(utilities.eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-  assertions.assertEqual(utilities.eqArrays(null, null), true);
-  assertions.assertEqual(utilities.eqArrays(null, [1, 2, 3]), false);
-};
-
-runTests();
+describe("#eqArrays", () => {
+  it("[1 , 2 , 3] is equal to [1 , 2 , 3]", () => {
+    assert.isTrue(utilities.eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+  it("['1' , '2' , '3'] is equal to ['1' , '2' , '3']", () => {
+    assert.isTrue(utilities.eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+  });
+  it("null equals null", () => {
+    assert.isTrue(utilities.eqArrays(null, null));
+  });
+  it("[3 , 2 , 1] is not equal to [1 , 2 , 3]", () => {
+    assert.isFalse(utilities.eqArrays([1, 2, 3], [3, 2, 1]));
+  });
+  it("['1' , '2' , 3] is not equal to ['1' , '2' , '3']", () => {
+    assert.isFalse(utilities.eqArrays(["1", "2", "3"], ["1", "2", 3]));
+  });
+  it("[1 , 2 , 3, 4] is not equal to [1 , 2 , 3]", () => {
+    assert.isFalse(utilities.eqArrays([1, 2, 3], [1, 2, 3, 4]));
+  });
+  it("null is not equal to ['1' , '2' , '3']", () => {
+    assert.isFalse(utilities.eqArrays(null, [1, 2, 3]));
+  });
+});
