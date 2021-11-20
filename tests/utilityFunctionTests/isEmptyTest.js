2b572653b42d577c95ab03b51b8cef3f1b9d5c80
SamualM2021 <smartelli@zoho.com>
2021-11-20 09:46:30 -0500

    Refactored isEmptyTest to use Mocha and Chai

--- tests/utilityFunctionTests/isEmptyTest.js
+++ tests/utilityFunctionTests/isEmptyTest.js
@@ -1,12 +1,14 @@
-const { assert } = require("chai");
-const assertions = require("../../assertions");
+const assert = require('chai').assert;
 const utilities = require("../../utilityFunctions");
 
-const runTests = function() {
-  assertions.assertTrue(utilities.isEmpty([]));
-  assertions.assertTrue(utilities.isEmpty({}));
-  assertions.assertFalse(utilities.isEmpty({a: 1, b: 2}));
-
-}
-
-runTests();
+describe("#isEmpty", () => {
+  it("An empty array should be empty", () => {
+    assert.isTrue(utilities.isEmpty([]));
+  });
+  it("An empty object should be empty", () => {
+    assert.isTrue(utilities.isEmpty({}));
+  });
+  it("A non-empty array should not be empty", () => {
+    assert.isFalse(utilities.isEmpty({a: 1, b: 2}));
+  });
+});
